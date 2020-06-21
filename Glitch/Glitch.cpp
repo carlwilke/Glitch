f74cfd0d26ea3c3408e7e36db8888508d4796f86
Carl Wilke <carlwilke@outlook.com>
2020-06-21 13:15:39 +0100

    First commit

---
+++ Glitch/Glitch.cpp
@@ -0,0 +1,13 @@
+/**
+ * @file Glitch.cpp
+ *
+ * @brief Start file for the project
+ *
+ * @ingroup Glitch Game
+ * (Note: this needs exactly one @defgroup somewhere)
+ *
+ * @author Carl Wilke
+ * Contact: carl@nomad-coders.com
+ *
+ */
+
